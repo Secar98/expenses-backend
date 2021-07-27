5a5b1f0bb9f0254e626a77894ceaf89acfc16b6d
Sebastian Carlson <sebcar98@gmail.com>
2021-07-27 11:15:20 +0200

    fixed the port again

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Welcome to api!");
+  res.send("Welcome to api!!");
 });
 
 module.exports = router;
